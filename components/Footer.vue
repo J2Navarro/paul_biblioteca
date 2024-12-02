b49d7606d3af71f729efc822bd04b9f54fd4e134
J2Navarro <127622256+J2Navarro@users.noreply.github.com>
2024-12-02 16:30:17 -0500

    Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,29 +2,37 @@
    <v-img
         class="mx-auto my-6"
         max-width="228"
-        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
+        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimeSM5vYveAClc-XArjw13aAsLeZzRSG6KvN2FNwt3ZwaZUWe2JBYdjzh8HGnT93SdcFpKZ75-7ZsNencvY5LDwrmvjsid_xpdb7J1k9Tgo9xLLu8hto0YO-Uah54V4txweY-dTMtz_HYcNDul_sQGk-eZr7Rm3sVYi6rT-DUqw_EdLJveN5m21mwkG_8/w945-h600-p-k-no-nu/previewLOGO%20(1).png"
       ></v-img>
     <v-footer
       class="bg-indigo-lighten-1 text-center d-flex flex-column"
     >
       <div>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          :icon="icon"
-          class="mx-4"
-          variant="text"
-        ></v-btn>
+        
+        <v-btn icon color="white" href="https://www.instagram.com/jorgeandr3s/profilecard/?igsh=bGh0MHoxNW1mZ2d3" variant="outlined" class="ma-2">
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      
+        <v-btn icon color="white" href="https://www.facebook.com/jorgeandr3s?mibextid=ZbWKwL" variant="outlined" class="ma-2">
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+
+        <v-btn icon color="white" href="https://youtube.com/@jorgeandr3s?si=cbH2tkRoZzt2N_LH" variant="outlined" class="ma-2">
+          <v-icon>mdi-youtube</v-icon>
+        </v-btn>
       </div>
   
       <div class="pt-0">
-        Realmente no tengo ni idea cómo he hecho todo esto; en serio. No entiendo nada de esto, ni siquiera al profe que explica tan bien. Definitivamente la programación no está hecha para mi.
+        Universidad de Córdoba - Facultad de educación - Departamento de Informática
       </div>
   
       <v-divider></v-divider>
   
       <div>
-        {{ new Date().getFullYear() }} — <strong>Jorge Andrés</strong>
+        {{ new Date().getFullYear() }} — <strong>Jorge Andrés</strong> 
+        <!--<v-btn icon color="white" href="/admin" variant="outlined" class="ma-2">
+          <v-icon>mdi-account</v-icon>
+        </v-btn>-->
       </div>
     </v-footer>
   </template>
