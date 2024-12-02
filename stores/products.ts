cc8cf0088fde1dd8fd68ad75a9b620f4ad58b874
J2Navarro <127622256+J2Navarro@users.noreply.github.com>
2024-12-02 16:36:56 -0500

    products.ts

--- stores/products.ts
+++ stores/products.ts
@@ -5,6 +5,7 @@ interface Product {
   title: string;
   price: number;
   description: string;
+  imageUrl: string;
 }
 
 export const useProductStore = defineStore({
@@ -25,5 +26,17 @@ export const useProductStore = defineStore({
       const products = await $fetch("/api/products");
       this.products = products;
     },    
-  },
+  updateProduct(updatedProduct: Product) {
+    const index = this.products.findIndex(product => product.id === updatedProduct.id);
+    if (index !== -1) {
+      this.products[index] = updatedProduct;  // Actualizar el producto en el array
+    }
+    },
+  updateProductImage(productId: number, imageUrl: string) {
+    const index = this.products.findIndex(product => product.id === productId);
+    if (index !== -1) {
+      this.products[index].imageUrl = imageUrl;  // Actualiza solo la URL de la imagen
+    }
+  }
+},
 });
